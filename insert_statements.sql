d952a976cd59569547d8d9313b646fb87b01457e
Leo Ho <hominhloc666@gmail.com>
2022-04-07 00:05:27 -0400

    edit insert

--- insert_statements.sql
+++ insert_statements.sql
@@ -1,7 +1,7 @@
 INSERT INTO airline(name) VALUES ("China Eastern");
 
 INSERT INTO airport(code, name, city, country, airport_type) VALUES 
-    ("JFK", "John F. Kennedy Airport", "USA", "International"),
+    ("JFK", "John F. Kennedy Airport", "New York", "USA", "International"),
     ("TIA", "Tribhuvan International Airport", "Kathmandu", "Nepal", "International"),
     ("HAN", "Noi Bai International Airport", "Hanoi", "Vietnam", "International"),
     ("PVG", "Shanghai Pudong International Airport", "Shanghai", "China", "International");
@@ -26,3 +26,10 @@ INSERT INTO flight(flight_num, airplane_ID, airline_name, base_price, status, de
 INSERT INTO ticket (ID_num, email, airplane_ID, airline_name, flight_num, departure_datetime, sold_price, card_number, name_on_card, card_type, expiration_date, purchase_datetime) VALUES 
     ('1', 'janedoe@gmail.com', '2', 'China Eastern', 'CE506', '2022-04-07 22:30:00', '230', '123412323123', 'Jane Doe', 'Credit', '2024-04-25', '2022-04-01 21:16:18'),
     ('2', 'johndoe@gmail.com', '1', 'China Eastern', 'CE707', '2022-04-06 11:30:00', '500', '124567348574', 'John Doe', 'Debit', '2024-04-17', '2022-03-17 21:16:18'),
+
+
+INSERT INTO purchase (ticket_ID, email) VALUES 
+    (1, 'janedoe@gmail.com'),
+    (2, 'johndoe@gmail.com');
+
+INSERT INTO staff_phone (username, phone_number) VALUES (leomessi, 3472312832); 
